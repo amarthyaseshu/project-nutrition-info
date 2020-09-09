c72d5e8b61e1a8a6a89ad15b4a88f6c4f05dfa1b
Amarthya <amarthyaseshu2@gmail.com>
2020-09-09 17:08:25 +0530

    FSJ0320B006

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const app = express();
 
 // Serve only the static files form the dist directory
 // Replace the '/dist/<to_your_project_name>'
-app.use(express.static(__dirname + '/dist/angular-nutritioninfo'));
+app.use(express.static(__dirname + '/dist/angular-nutrition-info'));
 
 app.get('*', function (req, res) {
   // Replace the '/dist/<to_your_project_name>/index.html'
-  res.sendFile(path.join(__dirname + '/dist/angular-nutritioninfo/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/angular-nutrition-info/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
